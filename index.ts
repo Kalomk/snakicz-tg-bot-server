0c64235b77d63c951c149b8bbaaf48a8f910b5c6
Denys <denya@MacBook-Air-Anastasia.local>
2023-11-29 18:06:46 +0100

    user-saved

--- index.ts
+++ index.ts
@@ -92,6 +92,10 @@ app.get('/', async (req, res) => {
   res.send('Hello world');
 });
 
+app.get('/open', async (req, res) => {
+  res.send('Open');
+});
+
 // Start the Express server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server started on PORT: ${PORT}`));
