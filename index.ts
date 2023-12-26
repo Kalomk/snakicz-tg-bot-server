29752080b6f481318d6c14c98d91f50cd7ed312c
Denys <denya@MacBook-Air-Anastasia.local>
2023-12-26 13:04:48 +0100

    app-use

--- index.ts
+++ index.ts
@@ -23,13 +23,11 @@ export const prisma = new PrismaClient();
 
 // Middleware
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 
-// Enable CORS for all routes
+// Enable CORS for all origins (not recommended for production)
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
