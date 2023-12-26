f8c6727b6a53b41966fbcb5f01684ba6a45ecc56
Denys <denya@MacBook-Air-Anastasia.local>
2023-12-26 12:57:01 +0100

    app-use

--- index.ts
+++ index.ts
@@ -25,6 +25,14 @@ export const prisma = new PrismaClient();
 app.use(express.json());
 app.use(cors());
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Create Telegram Bot
 export const bot: TelegramBot = new TelegramBot(_token, { polling: true });
 bot.on('polling_error', console.log);
