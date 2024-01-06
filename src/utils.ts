307ba28684b76debfaf7d3efa8a4b51e315b6741
Denys <denya@MacBook-Air-Anastasia.local>
2024-01-06 16:14:57 +0100

    bot-change23

--- src/utils.ts
+++ src/utils.ts
@@ -28,7 +28,7 @@ export function useControllers(app: express.Application) {
   const controllers = fs.readdirSync(path.join(__dirname, 'src', 'controllers'));
 
   controllers.forEach((controller) => {
-    const controllerModule = require(`./src/controllers/${controller}`);
+    const controllerModule = require(`./controllers/${controller}`);
 
     // Extracting variables from the controller file
     const variables = Object.keys(controllerModule);
