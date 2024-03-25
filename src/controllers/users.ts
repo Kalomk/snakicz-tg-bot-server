f1158e1e22ca7f1f33b7657f2047fc15a20a763a
Denys <denya@MacBook-Air-Anastasia.local>
2024-03-25 02:54:51 +0100

    secret-key

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -22,7 +22,7 @@ const getUsers: ControllerFunctionType = async (req, res) => {
     // Fetch total count of orders
     const totalCount = await prisma.user.count();
 
-    if (start - totalCount <= 0 && parsedPageCount > 1) {
+    if (totalCount - start <= 0 && parsedPageCount > 1) {
       return res.json([]);
     }
 
